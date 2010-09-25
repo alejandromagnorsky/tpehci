4be30eb5fa79543093cd88ea3c135775bde875b1
alejandromagnorsky <alejandromagnorsky@709b8525-212d-a56c-bbe7-f30a238506f4>
2010-09-25 15:54:44 +0000

    Calendario en español e inglés

--- js/register.js
+++ js/register.js
@@ -1,10 +1,10 @@
-window.onload = prepareButtons;
+window.onload = load;
 
-function prepareButtons(){
+function load(){
 	document.getElementById("buttonCancel").onclick = function(){
 		back();
 	};
-
+	
 	$("#datepicker").datepicker({
 		changeMonth: true,
 		changeYear: true,
@@ -12,4 +12,34 @@ function prepareButtons(){
 		minDate: '-100y',
 		maxDate: '+0d'
 	});
+	
+	var lang = 'en';
+	
+	if (lang == 'es') {
+		jQuery(function($){
+			$.datepicker.regional['es'] = {
+				closeText: 'Cerrar',
+				prevText: '&#x3c;Ant',
+				nextText: 'Sig&#x3e;',
+				currentText: 'Hoy',
+				monthNames: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+				monthNamesShort: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'],
+				dayNames: ['Domingo', 'Lunes', 'Martes', 'Mi&eacute;rcoles', 'Jueves', 'Viernes', 'S&aacute;bado'],
+				dayNamesShort: ['Dom', 'Lun', 'Mar', 'Mi&eacute;', 'Juv', 'Vie', 'S&aacute;b'],
+				dayNamesMin: ['Do', 'Lu', 'Ma', 'Mi', 'Ju', 'Vi', 'S&aacute;'],
+				weekHeader: 'Sm',
+				dateFormat: 'dd/mm/yy',
+				firstDay: 1,
+				isRTL: false,
+				showMonthAfterYear: false,
+				yearSuffix: ''
+			};
+			$.datepicker.setDefaults($.datepicker.regional['es']);
+		});
+	}
+	else {	
+		$.datepicker.setDefaults($.datepicker.regional['en']);
+	}
 }
+
+
