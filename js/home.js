a3cf87250aad79353a4d61c674e311d3896d1865
merchante.mariano <merchante.mariano@709b8525-212d-a56c-bbe7-f30a238506f4>
2010-10-01 18:42:30 +0000

    Muchos cambios de diseño.

--- js/home.js
+++ js/home.js
@@ -3,25 +3,33 @@
  */
 
 window.onload = loadMain;
-var headerMinPos;
+document.onclick = mouseClicked;
+
+
+var main;
+
 
 function loadMain() {
-	
-	headerMinPos = -200;
-	
+
+	main = true;
+
 	document.getElementById("spanlogin").onclick = showHideLogin;
 	document.getElementById("spanregister").onclick = showRegisterDialog;
 	document.getElementById("searchButton").onclick = slideHeaderUp;
 	document.getElementById("inputsearch").onclick = clearSearchData;
 	document.getElementById("homeLink").onclick = slideHeaderDown;
 	loadRegisterForm();
-	
-	$( "#menuCategorias" ).accordion();
+
+	$("#menuCategorias").accordion();
 }
 
-function clearSearchData(){
-	document.getElementById("inputsearch").value ="";
-	
+function clearSearchData() {
+	document.getElementById("inputsearch").value = "";
+
+}
+
+function mouseClicked(e) {
+	checkLogin(e);
 }
 
 function showRegisterDialog() {
@@ -34,58 +42,96 @@ function showRegisterDialog() {
 		"title" : "Registraci&oacute;n",
 		draggable : false
 	});
-	
-	
-	 var widget= $("#divRegister").dialog("widget");
-	 widget.css("margin","auto");
-	 widget.css("margin-top","0");
 
-	 widget.css("left","0");
-	 widget.css("right","0");
-	 
-	 widget.css("top","50px");
-	 widget.css("bottom","0");
-	 widget.css("height","750px");
-	 widget.css("width","600px");
-	 widget.css("text-align","left");
-	 
-	 widget.css("position","absolute");
-	 widget.css("-moz-box-shadow", " 0 0px 20px rgba(0, 0, 0, 1)");
-	 widget.css("-moz-border-radius","10px 10px 10px 10px");
-	 widget.css("background-color","transparent");
-	 widget.css("border","2px solid rgba(255,255,255,0.3)");
-	 
-	 /*
-		 * var overlay = $(".ui-widget-overlay");
-		 * overlay.css("background-color", "#000");
-		 */
+	var widget = $("#divRegister").dialog("widget");
+	widget.css("margin", "auto");
+	widget.css("margin-top", "0");
+
+	widget.css("left", "0");
+	widget.css("right", "0");
+
+	widget.css("top", "50px");
+	widget.css("bottom", "0");
+	widget.css("height", "750px");
+	widget.css("width", "600px");
+	widget.css("text-align", "left");
+
+	widget.css("position", "absolute");
+	widget.css("-moz-box-shadow", " 0 0px 20px rgba(0, 0, 0, 1)");
+	widget.css("-moz-border-radius", "10px 10px 10px 10px");
+	widget.css("background-color", "transparent");
+	widget.css("border", "2px solid rgba(255,255,255,0.3)");
+
+	/*
+	 * var overlay = $(".ui-widget-overlay"); overlay.css("background-color",
+	 * "#000");
+	 */
 }
 
+function slideHeaderUp() {
+	
+	if(main != true )
+		return;
+	
+	$("#divfisheye").fadeOut(500);
 
-function slideHeaderUp(){
-	
-	$("#header").animate({"height": "150px"}, 1000);
-	$("#headerContent").animate({"top": "-150px"}, 1000);
-	$("#headerTitle").animate({"top": "135px", "left": "-215px"}, 1000);
-	$("#titleImg").animate({ "height": "175px"}, 1000);
-	
-	$("#divfisheye").fadeOut(1000, function() {
-			$("#pageWrapper").css("display","block");
+	$("#headerBorder").fadeOut(500, function() {
+
+		$("#header").animate( {
+			"height" : "150px"
+		}, 1000);
+		$("#headerContent").animate( {
+			"top" : "-150px"
+		}, 1000);
+		$("#headerTitle").animate( {
+			"top" : "145px",
+			"left" : "-215px"
+		}, 1000);
+
+		$("#titleImg").animate( {
+			"height" : "150px"
+		}, 1000, function() {
+
+			$("#headerBorder").fadeIn(1000);
+			$("#pageWrapper").css("display", "block");
 			
-			
+			main = false;
 			// ACA CARGA LA PAGINA
-		}); 
+		});
+	});
 
 }
 
-function slideHeaderDown(){
+function slideHeaderDown() {
 	
-	$("#header").animate({"height": "300px"}, 1000);
-	$("#headerContent").animate({"top": "0px"}, 1000);
-	$("#headerTitle").animate({"top": "-20px", "left" : "0"	}, 1000);
-	$("#titleImg").animate({ "height": "300px"}, 1000);
-	
-	$("#pageWrapper").css("display","none");
-	
-	$("#divfisheye").fadeIn("slow");
+	if(main == true)
+		return;
+
+	$("#headerBorder").fadeOut(500, function() {
+
+		$("#header").animate( {
+			"height" : "300px"
+		}, 1000);
+		$("#headerContent").animate( {
+			"top" : "0px"
+		}, 1000);
+		$("#headerTitle").animate( {
+			"top" : "50px",
+			"left" : "20px"
+		}, 1000);
+		$("#titleImg").animate( {
+			"height" : "225px"
+		}, 1000);
+		
+		$("#pageWrapper").css("display", "none");
+
+		setTimeout(function() {
+
+			$("#divfisheye").fadeIn("slow");
+			$("#headerBorder").fadeIn(1000);
+			
+			main = true;
+		}, 1000);
+
+	});
 }
