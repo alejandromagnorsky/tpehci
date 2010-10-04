9c6c166180cd9911c7cbe3a586176777c6f6f7e0
alejandromagnorsky <alejandromagnorsky@709b8525-212d-a56c-bbe7-f30a238506f4>
2010-10-04 18:13:45 +0000

    Varios cambios importantes a la parte de idiomas

--- trunk/js/register.js
+++ trunk/js/register.js
@@ -2,118 +2,8 @@
 function loadRegisterForm(){
       
     var language = 1;
-    $("#divRegister").html(""); // Clear the divregister tag
-    var out = '';
-	
-    if (language == 1) {
-        out += '<form action="">';
-        out += 	'<div id = "personal">';
-        out += 		'<div class="regFormHeader">';
-       	out +=			 'Datos personales';
-        out += 		'</div>';
-        out += 		'<div class="regFormContent">';
-        out += 			'<div class="regInput">';
-        out += 				'<label class="regFormLabel">';
-        out += 					'<strong>Nombre/s*:</strong>';
-        out += 				'</label>';
-        out += 				'<br/>';
-        out += 				'<input name="name" type="text" size="40"/>';
-        out += 				'<br/>';
-        out += 				'<label class="regWarning">';
-        out +=					'Introduzca uno o m&aacute;s nombres';
-        out += 				'</label>';
-        out += 			'</div>';
-        out += 			'<div class="input">';
-        out += 				'<label class="regFormLabel">';
-        out += 					'<strong>Apellido/s*:</strong>';
-        out += 				'</label>';
-        out += 				'<br/>';
-        out += 				'<input name="sirname" type="text" size="40" />';
-        out += 				'<br/>';
-        out += 				'<label class="regWarning">';
-        out += 					'Introduzca uno o m&aacute;s apellidos';
-        out += 				'</label>';
-        out += 			'</div>';
-        out += 			'<div class="input">';
-        out += 				'<label class="regFormLabel">';
-        out += 					'<strong>Fecha de nacimiento*:</strong>';
-        out += 				'</label>';
-        out += 				'<br/>';
-        out += 				'<input id="datepicker" name="birthday" type="text" size="8" maxlength="10" />';
-        out += 			'</div>';
-        out += 			'<div class="input">';
-        out += 				'<label class="regFormLabel">';
-        out += 					'<strong>Pa&iacute;s*:</strong>';
-        out += 				'</label>';
-        out += 				'<br/>';
-        out += 				'<select name="country">';
-        out += 					'<option value="0" selected="selected">Argentina</option>';
-        out += 					'<option value="1">Estados Unidos</option>';
-        out += 					'<option value="2">Espa&ntilde;a</option>';
-        out += 					'<option value="3">Inglaterra</option>';
-        out += 				'</select>';
-        out += 			'</div>';
-        out +=	 		'<div class="input">';
-        out +=	 			'<label class="regFormLabel">';
-        out += 					'<strong>Provincia*:</strong>';
-        out += 				'</label>';
-        out += 				'<br/>';
-        out += 				'<select name="state">';
-        out +=	 				'<option value="0" selected="selected">Buenos Aires</option>';
-        out += 					'<option value="1">Misiones</option>';
-        out += 					'<option value="2">C&oacute;rdoba</option>';
-        out += 					'<option value="3">La Pampa</option>';
-        out += 				'</select>';
-        out += 			'</div>';
-        out += 		'</div>';
-        out += 	'</div>';
-        out += 	'<div id = "userData">';
-        out += 		'<div class="regFormHeader">';
-        out +=			'Datos de usuario';
-        out += 		'</div>';
-        out += 		'<div class="regFormContent">';
-        out += 			'<div class="regInput">';
-        out += 				'<label class="regFormLabel">';
-        out += 					'<strong>Nombre de usuario*:</strong>';
-        out += 				'</label>';
-        out += 				'<br/>';
-        out += 				'<input name="username" type="text" size="30" maxlength="20"/>';
-        out += 				'<br/>';
-        out += 			'</div>';
-        out += 			'<div class="input">';
-        out += 				'<label class="regFormLabel">';
-        out += 					'<strong>Contrase&ntilde;a*:</strong>';
-        out += 				'</label>';
-        out += 				'<br/>';
-        out += 				'<input name="password" type="password" size="20" maxlength="20"/>';
-        out += 				'<br/>';
-        out += 			'</div>';
-        out += 			'<div class="input">';
-        out += 				'<label class="regFormLabel">';
-        out += 					'<strong>Confirmar contrase&ntilde;a*:</strong>';
-        out += 				'</label>';
-        out += 				'<br/>';
-        out += 				'<input name="confirmPassword" type="password" size="20" maxlength="20"/>';
-        out += 				'<br/>';
-        out += 			'</div>';
-        out += 			'<div class="regInput">';
-        out += 				'<label class="regFormLabel">';
-        out += 					'<strong>Correo electr&oacute;nico*:</strong>';
-        out += 				'</label>';
-        out += 				'<br/>';
-        out += 				'<input name="email" type="text" size="30" maxlength="30"/>';
-        out += 				'<br/>';
-        out += 			'</div>';
-        out += 		'</div>';
-        out += 	'</div>';
-        out += 	'<div>';
-        out += 		'<label class="regReference">';
-        out += 			'<strong>*Datos obligatorios</strong>';
-        out += 		'</label>';
-        out += 		'<br/>';
-        out += 		'<input id="buttonOK" type="submit" value="Aceptar"/><input id="buttonCancel" type="button" value="Cancelar"/>';
-        out += 	'</div>';
-        out += '</form>';
+   	
+    if (language == 1) {      
         jQuery(function($){
             $.datepicker.regional['es'] = {
                 closeText: 'Cerrar',
@@ -136,9 +26,9 @@ function loadRegisterForm(){
         });
     }
     else if (language == 2) {
-     	$.datepicker.setDefaults($.datepicker.regional['en']);
+		$.datepicker.setDefaults($.datepicker.regional['en']);
     }
-	$("#divRegister").html(out);
+	
 	$("#datepicker").datepicker({
         changeMonth: true,
         changeYear: true,
