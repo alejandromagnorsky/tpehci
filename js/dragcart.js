2a5acdb8975ec9aee74ce6b862ed74f7cb2cea98
LegionarioDeAcero <LegionarioDeAcero@709b8525-212d-a56c-bbe7-f30a238506f4>
2010-10-04 03:20:36 +0000

    Eliminé un par de cosas agregué otro par. Esquema AJAX prácticamente definivo. loadcatalog.js se va a convertir en load.js y va a tener toda las funcion de get y post con el server. El cart anda bien pero le sacó un ALERT y deja de andar. Mañana (hoy) veo eso

--- js/dragcart.js
+++ js/dragcart.js
@@ -1,27 +1,28 @@
 $CART_ITEM_WIDTH = "83px";
 $CART_ITEM_HEIGHT = "112px";
 
-$(function(){
-	/* ********* PARCHAZO!!! ***************** */
-	while($("li", $products).length == 0);
-	/*  *************************************** */
+/* Two main objects: products and cart. */
+var products;
+var cart;
+
+function initCart(){
+	//alert($('#products > li').length);
+	products = $("#products");
+	cart = $("#cart");
 	
-	$(".tabs").tabs();
-	
-	/* Instantiates dialog windows for every product */
+	$(".tabs").tabs();		
+		
+	// Instantiates dialog windows for every product 
 	$(".description").dialog({
 			autoOpen: false, 
 			width: 1024,
 			resizable: false,
 			modal: true
 	});
-				
-    /* Two main object: products and cart. */
-    var $products = $("#products"), $cart = $("#cart");
 	
-    /* Products are draggable. */
-    $("li", $products).draggable({
-        cancel: "a.icon",	// Clicking an icon won't start drag.
+    // Products are draggable. 
+    $("li", products).draggable({
+        cancel: "a.icon",	// Clicking an icon won't start dragging.
         revert: "invalid",	// When dragged but not dropped, item reverts to initial position
         
 		/* DESPUES FIJARME BIEN SI SACÁNDOLA NO PASA NADA */
@@ -31,92 +32,17 @@ $(function(){
 		opacity: 0.8
     });
 
-    /* Cart is the dropzone. Products can be dropped in the cart. */
-    $cart.droppable({
+    // Cart is the dropzone. Products can be dropped into the cart.
+    cart.droppable({
         accept: "#products > li",
 		activeClass: "state-highlight",
         drop: function(event, ui){ addToCart(ui.draggable); }
     });
 	
+	/* DESPUES FIJARME BIEN SI SACÁNDOLA NO PASA NADA */
+	$( "<ul class='products helper-reset'/>" ).appendTo( cart );
 	
-	/* NECESITO SABER QUE MIERDA HACE ESTA LINEA Y POR QUE LA NECESITO, porque la necesito si o si. */
-	$( "<ul class='products helper-reset'/>" ).appendTo( $cart );
-	
-	/* Builds cart item */
-	function buildCartItem(item){
-		var addedItem = item.clone();
-		
-		/* Add some buttons. */
-		addedItem.find("a.icon-cart").remove();
-		addedItem.append("<a href='link/to/recycle/script/when/we/have/js/off' title='-' class='icon icon-minus'>- Quantity</a>");
-		addedItem.append("<a href='link/to/recycle/script/when/we/have/js/off' title='+' class='icon icon-plus'>+ Quantity</a>");
-		addedItem.append("<a href='link/to/recycle/script/when/we/have/js/off' title='Remove from cart' class='icon icon-remove'>Remove From Cart</a>");
-		addedItem.append("<span class='quantity'>1</span>");
-		
-		// Event delegation for cart items.
-		addedItem.click(function( event ) {
-			$target = $(event.target);
-			if ( $target.is( "a.icon-zoom" ) ) {
-				productDetails($target);
-			} else if ( $target.is( "a.icon-remove" ) ) {
-				removeFromCart($(this));
-			} else if ( $target.is( "a.icon-plus" ) ) {
-				var qty = parseInt(addedItem.find("span.quantity").text()) + 1;
-				addedItem.find("span.quantity").text(qty);
-			} else if ( $target.is( "a.icon-minus" ) ) {
-				var qty = parseInt(addedItem.find("span.quantity").text()) - 1;
-				if (qty <= 0){
-					removeFromCart($(this));
-				} else {
-					addedItem.find("span.quantity").text(qty);
-				}
-			}
-			return false;
-		});
-		return addedItem;
-	}
-	
-	/* Checks if an item is already in the cart */
-	function inCart(item){
-		var cartItems = $("ul", $cart).children();
-		for (var i=0; i<cartItems.length; i++){
-			if (cartItems[i].id == item[0].id){
-				return i;
-			}
-		}
-		return -1;
-	}
-	
-	function addToCart($item){
-		var i = inCart($item);
-		if (i >= 0){
-			var cartItems = $("ul", $cart).children();
-			var item = $("span", cartItems[i]);
-			var qty = parseInt(item.text()) + 1;
-			item.text(qty);
-		} else {
-			var itemToAdd = buildCartItem($item);
-			itemToAdd.appendTo($("ul", $cart)).fadeIn(function(){
-				itemToAdd.animate({ width: $CART_ITEM_WIDTH });
-				itemToAdd.children("img").animate({ height: $CART_ITEM_HEIGHT });;
-			});
-		}
-	}
-	
-	/* Removes an element from the cart. */
-	function removeFromCart($item){
-		$item.fadeOut( function(){ $("ul", $cart).children().remove("#" + $item[0].id); });
-	}
-    
-    /* View full product details */
-	function productDetails($link){
-		var toOpen =  $("#description" + $link.parent().attr("id"));
-		toOpen.dialog("option", "title", $link.siblings("img").attr("alt"));
-		toOpen.dialog("open");		
-		return false;
-	}
-	
-	/* Event delegation for products. */
+	// Event delegation for products.
 	$( "#products > li" ).click(function( event ) {
 		var $item = $(this);
 		$target = $( event.target );
@@ -128,4 +54,79 @@ $(function(){
 		}
 		return false;
 	});
-})
+}
+
+/* Builds cart item */
+function buildCartItem(item){
+	var addedItem = item.clone();
+	
+	// Add some buttons.
+	addedItem.find("a.icon-cart").remove();
+	addedItem.append("<a href='link/to/recycle/script/when/we/have/js/off' title='-' class='icon icon-minus'>- Quantity</a>");
+	addedItem.append("<a href='link/to/recycle/script/when/we/have/js/off' title='+' class='icon icon-plus'>+ Quantity</a>");
+	addedItem.append("<a href='link/to/recycle/script/when/we/have/js/off' title='Remove from cart' class='icon icon-remove'>Remove From Cart</a>");
+	addedItem.append("<span class='quantity'>1</span>");
+	
+	// Event delegation for cart items.
+	addedItem.click(function( event ) {
+		$target = $(event.target);
+		if ( $target.is( "a.icon-zoom" ) ) {
+			productDetails($target);
+		} else if ( $target.is( "a.icon-remove" ) ) {
+			removeFromCart($(this));
+		} else if ( $target.is( "a.icon-plus" ) ) {
+			var qty = parseInt(addedItem.find("span.quantity").text()) + 1;
+			addedItem.find("span.quantity").text(qty);
+		} else if ( $target.is( "a.icon-minus" ) ) {
+			var qty = parseInt(addedItem.find("span.quantity").text()) - 1;
+			if (qty <= 0){
+				removeFromCart($(this));
+			} else {
+				addedItem.find("span.quantity").text(qty);
+			}
+		}
+		return false;
+	});
+	return addedItem;
+}
+
+/* Checks if an item is already in the cart */
+function inCart(item){
+	var cartItems = $("ul", cart).children();
+	for (var i=0; i<cartItems.length; i++){
+		if (cartItems[i].id == item[0].id){
+			return i;
+		}
+	}
+	return -1;
+}
+
+/* Adds item to the cart. */
+function addToCart($item){
+	var i = inCart($item);
+	if (i >= 0){
+		var cartItems = $("ul", cart).children();
+		var item = $("span", cartItems[i]);
+		var qty = parseInt(item.text()) + 1;
+		item.text(qty);
+	} else {
+		var itemToAdd = buildCartItem($item);
+		itemToAdd.appendTo($("ul", cart)).fadeIn(function(){
+			itemToAdd.animate({ width: $CART_ITEM_WIDTH });
+			itemToAdd.children("img").animate({ height: $CART_ITEM_HEIGHT });;
+		});
+	}
+}
+
+/* Removes an element from the cart. */
+function removeFromCart($item){
+	$item.fadeOut( function(){ $("ul", cart).children().remove("#" + $item[0].id); });
+}
+   
+/* View full product details */
+function productDetails($link){
+	var toOpen =  $("#description" + $link.parent().attr("id"));
+	toOpen.dialog("option", "title", $link.siblings("img").attr("alt"));
+	toOpen.dialog("open");		
+	return false;
+}
--- js/loadcatalog.js
+++ js/loadcatalog.js
@@ -1,27 +1,51 @@
-window.onload = getXMLHttpRequest();
+/* API Services */
+$COMMON = '/service/Common.groovy?method=';
+$SECURITY = '/service/Security.groovy?method=';
+$CATALOG = '/service/Catalog.groovy?method=';
+$ORDER = '/service/Order.groovy?method=';
 
-function getXMLHttpRequest(){
-	var request = new XMLHttpRequest();
-	if(request) {
-		getProductListByCategory(request, 1, 1, 'ASC', 10, 1);
+/* Images */
+$IMG_WIDTH = 250;
+$IMG_HEIGHT = 360;
+
+/* Thumbs */
+$THUMB_WIDTH = 96;
+$THUMB_HEIGHT = 125;
+
+/* Misc */
+$JS_OFF = 'link/al/cart/cuando/js/esta/off';
+
+var request;
+
+/* THIS IS OUR MAIN FUNCTION. THIS FUNCTION EXECUTES BEFORE THE DAWN OF TIME. */
+$(function(){
+	request = new XMLHttpRequest();
+	requestFromServer('GetProductListByCategory', 'language_id=1&category_id=1&order=ASC&items_per_page=10&page=1');
+});
+
+function requestFromServer(method, parameters){
+	if ( method == 'GetProductListByCategory' ){
+		getProductListByCategory(parameters);
+		
+		// QUÉ CARAJO ESTA PASANDO ACÁ!!!!!!!
+		alert($("ul#products").length);
+		/////////////////////////
+		
+		initCart();
 	}
 }
 
-function getProductListByCategory(request, language_id, category_id, order, items_per_page, page){
-	var url = '/service/Catalog.groovy?method=GetProductListByCategory';
-	url += '&language_id=' + language_id;
-	url += '&category_id=' + category_id;
-	url += '&order=' + order;
-	url += '&items_per_page=' + items_per_page;
-	url += '&page=' + page;
+function getProductListByCategory(parameters){
+	var url = $CATALOG + 'GetProductListByCategory' + '&' + parameters;
 	request.open('GET', url, true);
 	request.onreadystatechange = function(){
 		if (request.readyState == 4) {
 			if (request.status == 200) {
 				var response = request.responseXML;
-				var paramList= response.getElementsByTagName('category');  alert("3");
+				var paramList= response.getElementsByTagName('category');
 				
-				var i=1, out = "";
+				var i=1, j=1;	// Id counter(i) and tab counter(j).
+				var out = "";
 				$(response).find('product').each(function() {
 					var marker = $(this);
 					var name = marker.find("name").text();
@@ -29,12 +53,12 @@ function getProductListByCategory(request, language_id, category_id, order, item
 					var price = marker.find("price").text();
 					out +=	'<li class="product-content corner-tr" id="' + i + '">'
 					out +=		'<h5 class="product-header">' + name + '</h5>';
-					out +=		'<div id="description' + i + '" class="description hide">';
-					out +=			'<img src="' + image_url + '" alt="' + name + '" width="250" height="360" class="image"></img>';
+					out +=		'<div id="description' + i++ + '" class="description hide">';
+					out +=			'<img src="' + image_url + '" alt="' + name + '" width="' + $IMG_WIDTH + '" height="' + $IMG_HEIGHT + '" class="image"></img>';
 					out +=			'<div class="tabs">';
-                   	out +=			    '<ol><li><a href="#tabs-1">Detalles</a></li>';
-					out +=				'<li><a href="#tabs-2">Sinopsis</a></li></ol>';
-					out +=				'<div id="tabs-1">';
+                   	out +=			    '<ol><li><a href="#tabs-' + j + '">Detalles</a></li>';
+					out +=				'<li><a href="#tabs-' + (j+1) + '">Sinopsis</a></li></ol>';
+					out +=				'<div id="tabs-' + j++ + '">';
 					out +=					'<div id="details">';
                     out +=						'<div class="divPrice">';            
 					out +=							'<p class="spanPrice">Precio por unidad: $' + price + '</p>';
@@ -43,7 +67,7 @@ function getProductListByCategory(request, language_id, category_id, order, item
 					out +=						'<div class="name">' + name + '<br /></div>';
 					out +=						'<div class="autor">';
 					out +=							'Director: Dario Argento<br/>';
-					out +=							'Cast: Jessica Harper, Stefania Casini, Flavio Bucci, Miguel Bose, Barabara Magnolfi<br/>';
+					out +=							'Cast: Jessica Harper, Stefania Casini, Flavio Bucci, Miguel Bose, Barbara Magnolfi<br/>';
 					out +=							'Wild Side Films<br/>';
 					out +=						'</div>';
 					out +=						'<div class="category">';
@@ -54,7 +78,7 @@ function getProductListByCategory(request, language_id, category_id, order, item
 					out +=						'</div>';
 					out +=					'</div>';
 					out +=				'</div>';
-					out +=				'<div id="tabs-2">';
+					out +=				'<div id="tabs-' + j++ + '">';
 					out +=					'<div class="sinopsis">';
 					out +=						'A young American dancer travels to Europe to join a famous ballet school. As she arrives,';
 					out +=						'the camera turns to another young woman, who appears to be fleeing from the school. She returns ';
@@ -66,17 +90,14 @@ function getProductListByCategory(request, language_id, category_id, order, item
 					out +=				'</div>';
 					out +=			'</div>';
 					out +=		'</div>';
-					out +=		'<img src="' + image_url + '" alt="' + name + '" width="96" height="125"/>';
+					out +=		'<img src="' + image_url + '" alt="' + name + '" width="' + $THUMB_WIDTH + '" height="' + $THUMB_HEIGHT + '"/>';
 					out +=		'<a href="description.html" title="Full product details" class="icon icon-zoom">Full product details</a>';
-					out +=		'<a href="link/to/trash/script/when/we/have/js/off" title="Add to cart" class="icon icon-cart">Add to cart</a>';
-					out +=	'</li>'
-					i++;					
+					out +=		'<a href="' + $JS_OFF + '" title="Add to cart" class="icon icon-cart">Add to cart</a>';
+					out +=	'</li>'			
 				});
-				$('#products').html(out);
-			}
-			else {
-				alert('Error:\n');
-				alert(request.statusText);
+				$('ul#products').html(out);
+			} else {
+				alert('Error: ' + request.statusText);
 			}
 			request = null;
 		}
