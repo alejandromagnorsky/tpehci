85d38fb92936b8b780c5a1934383b5e79be0faa3
LegionarioDeAcero <LegionarioDeAcero@709b8525-212d-a56c-bbe7-f30a238506f4>
2010-10-08 16:30:56 +0000

    Acá incluyo include.js. Disculpen si los retrasé en algo, pero pensé que el tortoise me lo habia seleccionado automáticamente para agregar

---
+++ trunk/js/include.js
@@ -0,0 +1,42 @@
+/* API Services **************************************************** */
+$COMMON = '/service/Common.groovy?method=';
+$SECURITY = '/service/Security.groovy?method=';
+$CATALOG = '/service/Catalog.groovy?method=';
+$ORDER = '/service/Order.groovy?method=';
+/* ***************************************************************** */
+
+/* CATALOG & SHOPPING CART ***************************************** */
+$ITEM_CONTAINER_TAG = 'ul';
+
+// Cart
+$CART = 'cart';
+$CART_HEADER = 'product-header';
+$CART_DEFAULT = 'state-default';
+$CART_HIGHLIGHT = 'state-highlight';
+
+// Product in catalog
+$CATALOG_ITEM = 'li';
+$CATALOG_ITEM_HEADER = 'product-header';
+$CATALOG_CONTAINER_ID = 'products';
+$CATALOG_ITEM_DESCRIPTION = 'description';
+$CATALOG_ITEM_QUANTITY = 'quantity';
+$IMG_WIDTH	= 250;	$THUMB_WIDTH  = 96;
+$IMG_HEIGHT	= 360;	$THUMB_HEIGHT = 125;
+
+// Product in cart
+$CART_ITEM_WIDTH = '83px';
+$CART_ITEM_HEIGHT = '112px';
+/* ***************************************************************** */
+
+/* ICON CLASSES **************************************************** */
+$ICON = 'icon';
+$ICON_CART = 'icon-cart';
+$ICON_INFO = 'icon-zoom';
+$ICON_PLUS = 'icon-plus';
+$ICON_MINUS = 'icon-minus';
+$ICON_REMOVE = 'icon-remove';
+/* ***************************************************************** */
+
+/* MISC ************************************************************ */
+$JS_OFF = 'JAVASCRIPT_IS_OFF';
+/* ***************************************************************** */
