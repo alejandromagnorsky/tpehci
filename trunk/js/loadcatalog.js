08344799a7046c94f14ba97037060d63b130ad57
LegionarioDeAcero <LegionarioDeAcero@709b8525-212d-a56c-bbe7-f30a238506f4>
2010-10-13 20:24:02 +0000

    ODIO AL MAN DE HCI Y A SU API APESTOSA. Ya lee "bien" de las categorias, y carga data segun sea libro o dvd. Bugs avisenme

--- trunk/js/loadcatalog.js
+++ trunk/js/loadcatalog.js
@@ -73,48 +73,63 @@ function getProductList(parameters) {
 				/* Resize content */
 				initializeContent(qty);
 				
-				
 				if(qty == 0 )
 					$('div.product').html("<h3>No se encontró ningun producto. Me gusta el jamon.</h3>");
 				else $('div.product').html("");	// Clear content
 				
 				var out = '<' + $ITEM_CONTAINER_TAG + ' id="' + $CATALOG_CONTAINER_ID + '" class="products helper-reset helper-clearfix"/>';
 				$('div.product').append(out);
+				alert($(response).find('response').attr('status'));
 				$(response).find('product').each( function() {
 					out = '';
 					var marker = $(this);
 					var name = marker.find("name").text();
 					var image_url = marker.find("image_url").text();
-					var price = marker.find("price").text();
-					out +=	'<' + $CATALOG_ITEM + ' class="product-content corner-tr" id="' + i + '">'
+					var price = marker.find("price").text();					
+					var c_id = marker.find("category_id").text();
+					var sc_id = marker.find("subcategory_id").text();
+					var genre = categories[c_id-1].name + ' | ' + (getSubCategoryName(sc_id-1));
+					if (c_id == 1)
+						var productResponse = getProduct($(this).attr('id'));
+					out +=	'<' + $CATALOG_ITEM + ' class="product-content corner-tr" id="' + i + c_id + sc_id + '">'
 					out +=  '<div class="productBg"/>'
-					//out +=		'<h5 class="' + $CATALOG_ITEM_HEADER + '">' + name + '</h5>';
-					out +=		'<div id="description' + i++ + '" class="' + $CATALOG_ITEM_DESCRIPTION + ' hide">';
+					out +=		'<h5 class="' + $CATALOG_ITEM_HEADER + '">' + name + '</h5>';
+					out +=		'<div id="description' + i++ + c_id + sc_id + '" class="' + $CATALOG_ITEM_DESCRIPTION + ' hide">';
 					out +=			'<img src="' + image_url + '" alt="' + name + '" width="' + $IMG_WIDTH + '" height="' + $IMG_HEIGHT + '" class="image"></img>';
 					out +=			'<div class="tabs">';
-					out +=				'<ol><li><a href="#tabs-' + j + '">Detalles</a></li>';
-					out +=				'<li><a href="#tabs-' + (j + 1) + '">Sinopsis</a></li></ol>';
-					out +=				'<div id="tabs-' + j++ + '">';
+					out +=				'<ol><li><a href="#tabs-' + j + c_id + sc_id + '">Detalles</a></li>';
+					out +=				'<li><a href="#tabs-' + (j + 1) + c_id + sc_id + '">Sinopsis</a></li></ol>';
+					out +=				'<div id="tabs-' + j++ + c_id + sc_id + '">';
 					out +=					'<div id="details">';
 					out +=						'<div class="divPrice">';
 					out +=							'<p class="spanPrice">Precio por unidad: $' + price + '</p>';
 					out +=							'<input id="addtocart" type="button" value=""/>';
 					out +=						'</div>';
 					out +=					'<div class="name">' + name + '<br /></div>';
+					if (c_id == 1)
+						out +=				'<div class="salesRank">' + ($(productResponse).find("sales_rank").text()) + '<br /></div>';
 					out +=					'<div class="autor">';
+					out +=						'Genre: ' + genre + '<br/>';
+					out +=						'MPAA Rating: RATING' + '<br/>';
 					out +=						'Director: Dario Argento<br/>';
-					out +=						'Cast: Jessica Harper, Stefania Casini, Flavio Bucci, Miguel Bose, Barbara Magnolfi<br/>';
-					out +=						'Wild Side Films<br/>';
+					if (c_id == 1)
+						out +=					'Cast: ' + ($(productResponse).find("actors").text()) + '<br/>';
 					out +=					'</div>';
 					out +=					'<div class="category">';
-					out +=						'Genre: CATEGORY' + '<br/>';
-					out +=						'Format: FORMAT' + '<br/>';
-					out +=						'Original release date: DATE' + '<br/>';
-					out +=						'Rating: RATING' + '<br/>';
+					if (c_id == 1){
+						out +=					'Spoken language: ' + ($(productResponse).find("language").text()) + '<br/>';
+						out +=					'Subtitles: ' + ($(productResponse).find("subtitles").text()) + '<br/>';
+						out +=					'Total runtime: ' + ($(productResponse).find("run_time").text()) + '<br/>';
+						out +=					'Release date: ' + ($(productResponse).find("release_date").text()) + '<br/>';
+						out +=					'Format: ' + ($(productResponse).find("format").text()) + '<br/>';
+						out +=					'Aspect ratio: ' + ($(productResponse).find("aspect_ratio").text()) + '<br/>';
+						out +=					'Number of discs: ' + ($(productResponse).find("number_discs").text()) + '<br/>';
+						out +=					'ASIN: ' + ($(productResponse).find("ASIN").text()) + '<br/>';
+					}
 					out +=					'</div>';
 					out +=				'</div>';
 					out +=			'</div>';
-					out +=			'<div id="tabs-' + j++ + '">';
+					out +=			'<div id="tabs-' + j++ + c_id + sc_id + '">';
 					out +=				'<div class="sinopsis">';
 					out +=					'A young American dancer travels to Europe to join a famous ballet school. As she arrives,';
 					out +=					'the camera turns to another young woman, who appears to be fleeing from the school. She returns ';
@@ -145,13 +160,22 @@ function getProductList(parameters) {
 	request.send();
 }
 
+function getProduct(product_id) {
+	var request = new XMLHttpRequest();
+	var url = $CATALOG + 'GetProduct' + '&product_id=' + product_id;
+	request.open('GET', url, false);
+	request.send();
+	if (request.status == 200)
+		return request.responseXML;
+	else
+		alert('Error: ' + request.statusText);
+}
+
 function getCategories(language) {
 	requestFromServer('GetCategoryList', language);
 
-	for ( var i = 0; i < categories.length; i++) {
-		requestFromServer('GetSubcategoryList', language + '&category_id='
-				+ (i + 1));
-	}
+	for ( var i = 0; i < categories.length; i++)
+		requestFromServer('GetSubcategoryList', language + '&category_id=' + (i + 1));
 }
 
 function getCategoryList(parameters) {
@@ -216,7 +240,7 @@ function injectCategories() {
 		out +=	'<h3><a class="categoryLink" href="#">' + categories[i].name + '</a></h3>';
 		out +=	'<div><li>';
 		for (j = 0; j < categories[i].subcategories.length; j++)
-			out +=	'<a class="subCategoryLink" href="#">' + categories[i].subcategories[j].name + '</a><br/>';
+			out +=	'<a class="subCategoryLink parentCategory' + (i+1) + '" href="#">' + categories[i].subcategories[j].name + '</a><br/>';
 		out +=	'</li></div>';
 	}
 	$("#menuCategorias").html(out);
@@ -230,10 +254,9 @@ function injectCategories() {
 		out +=		'</div>';
 		out +=		'<div class="categoryBlockSub">';
 		for (j = 0; j < categories[i].subcategories.length; j++)
-			out +=		'<a class="subCategoryLink" href="#">' + categories[i].subcategories[j].name + '</a>';
+			out +=		'<a class="subCategoryLink parentCategory' + (i+1) + '" href="#">' + categories[i].subcategories[j].name + '</a>';
 		out +=		'</div>';
 		out +=	'</div>';
-		//out +=	'</div>';	// ESTE DIV SE PUEDE ELIMINAR???? CREO QUE SI, PROBE Y NO CAMBIA NADA.
 	}
 	$("#categorySelector").html(out);
 	
@@ -241,11 +264,10 @@ function injectCategories() {
 	out = "";
 	for (i = 0; i < categories.length; i++) {
 		out +=	'<div class="footerBlock">';
-		out +=			'<a class="categoryLink" href="#">' + categories[i].name + '</a><br/>';
+		out +=		'<a class="categoryLink" href="#">' + categories[i].name + '</a><br/>';
 		for (j = 0; j < categories[i].subcategories.length; j++)
-			out +=		'<a class="subCategoryLink" href="#">' + categories[i].subcategories[j].name + '</a>';
+			out +=	'<a class="subCategoryLink parentCategory' + (i+1) + '" href="#">' + categories[i].subcategories[j].name + '</a>';
 		out +=	'</div>';
-		//out +=	'</div>';	// ESTE DIV SE PUEDE ELIMINAR???? CREO QUE SI, PROBE Y NO CAMBIA NADA.
 	}
 	$("#footerInfo").html(out);
 	
@@ -256,19 +278,26 @@ function injectCategories() {
 		$target = $( event.target );
 		if ($target.is( 'a.categoryLink' )){
 			$($ITEM_CONTAINER_TAG + '#' + $CATALOG_CONTAINER_ID).remove(); // Cleans old search.
+			alert("hola");
 			requestFromServer('GetProductList', 'Category&language_id=' + currentLang + '&category_id=' + (getCategoryIndex($target.html()) + 1) + '&order=ASC&items_per_page=10&page=1');
+			alert("CHAU");
 			slideHeaderUp();
 		} else if ($target.is( 'a.subCategoryLink' )){
-			var ids = getSubCategoryIndex($target.html());
+			var c_id, sc_id;
+			for(c_id=0; c_id < categories.length; c_id++)
+				if($target.is( 'a.parentCategory' + (c_id+1) ))
+					break;
+			sc_id = getSubCategoryIndex(c_id, $target.html()) + 1;
+			c_id++;
 			$($ITEM_CONTAINER_TAG + '#' + $CATALOG_CONTAINER_ID).remove(); // Cleans old search.
-			requestFromServer('GetProductList', 'Subcategory&language_id=' + currentLang + '&category_id=' + (ids[0]+1) + '&subcategory_id=' + (ids[1]+1) + '&order=ASC&items_per_page=10&page=1');
+			requestFromServer('GetProductList', 'Subcategory&language_id=' + currentLang + '&category_id=' + c_id + '&subcategory_id=' + sc_id + '&order=ASC&items_per_page=10&page=1');
 			slideHeaderUp();
 		}
 		return false;
 	});
 	
 
-	// Event delegation for categories in '#categorySelector'.
+	// Event delegation for categories in '#menuCategorias'.
 	$( '#menuCategorias' ).click(function( event ) {
 		var $item = $(this);
 		$target = $( event.target );
@@ -277,15 +306,20 @@ function injectCategories() {
 			requestFromServer('GetProductList', 'Category&language_id=' + currentLang + '&category_id=' + (getCategoryIndex($target.html()) + 1) + '&order=ASC&items_per_page=10&page=1');
 			slideHeaderUp();
 		} else if ($target.is( 'a.subCategoryLink' )){
-			var ids = getSubCategoryIndex($target.html());
+			var c_id, sc_id;
+			for(c_id=0; c_id < categories.length; c_id++)
+				if($target.is( 'a.parentCategory' + (c_id+1) ))
+					break;
+			sc_id = getSubCategoryIndex(c_id, $target.html()) + 1;
+			c_id++;
 			$($ITEM_CONTAINER_TAG + '#' + $CATALOG_CONTAINER_ID).remove(); // Cleans old search.
-			requestFromServer('GetProductList', 'Subcategory&language_id=' + currentLang + '&category_id=' + (ids[0]+1) + '&subcategory_id=' + (ids[1]+1) + '&order=ASC&items_per_page=10&page=1');
+			requestFromServer('GetProductList', 'Subcategory&language_id=' + currentLang + '&category_id=' + c_id + '&subcategory_id=' + sc_id + '&order=ASC&items_per_page=10&page=1');
 			slideHeaderUp();
 		}
 		return false;
 	});
 
-	// Event delegation for categories in '#categorySelector'.
+	// Event delegation for categories in '.footerBlock'.
 	$( '.footerBlock' ).click(function( event ) {
 		var $item = $(this);
 		$target = $( event.target );
@@ -294,9 +328,14 @@ function injectCategories() {
 			requestFromServer('GetProductList', 'Category&language_id=' + currentLang + '&category_id=' + (getCategoryIndex($target.html()) + 1) + '&order=ASC&items_per_page=10&page=1');
 			slideHeaderUp();
 		} else if ($target.is( 'a.subCategoryLink' )){
-			var ids = getSubCategoryIndex($target.html());
+			var c_id, sc_id;
+			for(c_id=0; c_id < categories.length; c_id++)
+				if($target.is( 'a.parentCategory' + (c_id+1) ))
+					break;
+			sc_id = getSubCategoryIndex(c_id, $target.html()) + 1;
+			c_id++;
 			$($ITEM_CONTAINER_TAG + '#' + $CATALOG_CONTAINER_ID).remove(); // Cleans old search.
-			requestFromServer('GetProductList', 'Subcategory&language_id=' + currentLang + '&category_id=' + (ids[0]+1) + '&subcategory_id=' + (ids[1]+1) + '&order=ASC&items_per_page=10&page=1');
+			requestFromServer('GetProductList', 'Subcategory&language_id=' + currentLang + '&category_id=' + c_id + '&subcategory_id=' + sc_id + '&order=ASC&items_per_page=10&page=1');
 			slideHeaderUp();
 		}
 		return false;
@@ -311,13 +350,23 @@ function getCategoryIndex(categoryName){
 	return -1;
 }
 
-function getSubCategoryIndex(subcategoryName){
-	var i, j, ans = new Array(0);
-	for(i = 0; i < categories.length; i++)
-		for(j = 0; j < categories[i].subcategories.length; j++)
-			if (categories[i].subcategories[j].name == subcategoryName){
-				ans.push(i); ans.push(j);
-				return ans;
-			}
+function getSubCategoryIndex(categoryId, subcategoryName){
+	var j;
+	var subcategoriesCount = 0;
+	for(j = 0; j < categoryId; j++)
+		subcategoriesCount += categories[j].subcategories.length;
+	for(j = 0; j < categories[categoryId].subcategories.length; j++)
+		if (categories[categoryId].subcategories[j].name == subcategoryName)
+			return subcategoriesCount + j;
+	return -1;
+}
+
+function getSubCategoryName(sc_id){
+	var i, j, k=0;
+	for(i=0; i < categories.length; i++){
+		for(j=0; j < categories[i].subcategories.length; j++, k++)
+			if (sc_id == k)
+				return categories[i].subcategories[j].name;
+	}
 	return null;
 }
